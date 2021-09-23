68500cf94109452c37239e0b5a894aec5430cdfd
edynson muñoz jimenez <edinsonmjim@unicauca.edu.co>
2021-09-23 14:21:18 -0500

    definicion de controladores faltantes

---
+++ application/controllers/GestionCarrito.php
@@ -0,0 +1,9 @@
+<?php
+class GestionCarrito extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('ModeloCarrito');
+    }
+}
