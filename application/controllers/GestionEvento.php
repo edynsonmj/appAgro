b4eef998b2e3cc422a881845a5c40cbeed6a400f
edynson muñoz jimenez <edinsonmjim@unicauca.edu.co>
2021-09-23 15:11:45 -0500

    definicion de funciones en los controladores

--- application/controllers/GestionEvento.php
+++ application/controllers/GestionEvento.php
@@ -1,9 +1,64 @@
 <?php
+require_once "/xampp/htdocs/appAgro/application/negocio/clsEvento.php";
 class GestionEvento extends CI_Controller
 {
     public function __construct()
     {
         parent::__construct();
         $this->load->model('ModeloEvento');
+    }
+    /**
+     * para obtener un Evento
+     */
+    public function getEvento($prmId){
+        //posiblemente tu codigo aqui
+        //EJEMPLO DE USO DEL MODELO
+        //crear objeto clsEvento
+        $auxEvento = new clsEvento();
+        //llamado al modelo
+        $auxEvento = $this->ModeloEvento->obtenerEvento($prmId);
+        //posiblemente tu codigo aqui
+    }
+    /**
+     * para obtener todos los Eventos
+     */
+    public function allEventos(){
+        //posiblemente tu codigo aqui
+        //EJEMPLO DE USO DEL MODELO
+        //recupera los Eventos en un rray para ello, ejemplo
+        $Eventos = array();
+        //llamado al modelo
+        $Eventos = $this->ModeloEvento->listarEventos();
+        //posiblemente tu codigo aqui
+    }
+    /**
+     * para eliminar un Evento
+     */
+    public function deleteEvento($prmId){
+        //posiblemente tu codigo aqui
+        //EJEMPLO DE USO DEL MODELO
+        //llamado al modelo
+        $this->ModeloEvento->eliminarEvento($prmId);
+        //posiblemente tu codigo aqui
+    }
+    /**
+     * para agregar un Evento
+     */
+    public function addEvento(clsEvento $prmEvento){
+        //posiblemente tu codigo aqui
+        //EJEMPLO DE USO DEL MODELO
+        //llamado al modelo
+        $this->ModeloEvento->agregarEvento($prmEvento);
+        //posiblemente tu codigo aqui
+    }
+    /**
+     * para actualizar un Evento
+     */
+    public function updateEvento(clsEvento $prmEvento){
+        //posiblemente tu codigo aqui
+        //EJEMPLO DE USO DEL MODELO
+        //llamado al modelo
+        $this->ModeloEvento->actualizarEvento($prmEvento);
+        //posiblemente tu codigo aqui
     }
 }
--- application/controllers/GestionOrganizacion.php
+++ application/controllers/GestionOrganizacion.php
@@ -1,10 +1,65 @@
 <?php
+require_once '/xampp/htdocs/appAgro/application/negocio/clsOrganizacion.php';
 class GestionOrganizacion extends CI_Controller
 {
     public function __construct()
     {
         parent::__construct();
         $this->load->model('ModeloOrganizacion');
+    }
+    /**
+     * para obtener una Organizacion
+     */
+    public function getOrganizacion($prmId){
+        //posiblemente tu codigo aqui
+        //EJEMPLO DE USO DEL MODELO
+        //crear objeto clsOrganizacion
+        $auxOrganizacion = new clsOrganizacion();
+        //llamado al modelo
+        $auxOrganizacion = $this->ModeloOrganizacion->obtenerOrganizacion($prmId);
+        //posiblemente tu codigo aqui
+    }
+    /**
+     * para obtener todas las Organizaciones
+     */
+    public function allOrganizaciones(){
+        //posiblemente tu codigo aqui
+        //EJEMPLO DE USO DEL MODELO
+        //recupera las Organizacions en un rray para ello, ejemplo
+        $Organizaciones = array();
+        //llamado al modelo
+        $Organizaciones = $this->ModeloOrganizacion->listarOrganizaciones();
+        //posiblemente tu codigo aqui
+    }
+    /**
+     * para eliminar una Organizacion
+     */
+    public function deleteOrganizacion($prmId){
+        //posiblemente tu codigo aqui
+        //EJEMPLO DE USO DEL MODELO
+        //llamado al modelo
+        $this->ModeloOrganizacion->eliminarOrganizacion($prmId);
+        //posiblemente tu codigo aqui
+    }
+    /**
+     * para agregar una Organizacion
+     */
+    public function addOrganizacion(clsOrganizacion $prmOrganizacion){
+        //posiblemente tu codigo aqui
+        //EJEMPLO DE USO DEL MODELO
+        //llamado al modelo
+        $this->ModeloOrganizacion->agregarOrganizacion($prmOrganizacion);
+        //posiblemente tu codigo aqui
+    }
+    /**
+     * para actualizar una Organizacion
+     */
+    public function updateOrganizacion(clsOrganizacion $prmOrganizacion){
+        //posiblemente tu codigo aqui
+        //EJEMPLO DE USO DEL MODELO
+        //llamado al modelo
+        $this->ModeloOrganizacion->actualizarOrganizacion($prmOrganizacion);
+        //posiblemente tu codigo aqui
     }
 }
 ?>
